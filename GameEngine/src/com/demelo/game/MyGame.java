5d906f685cb2a6009b5d1a29bd9b72a4123ef41e
melod <Daniel.Melo@lhsgroup.com>
2018-04-03 00:21:27 -0300

    My first game engine.

---
+++ GameEngine/src/com/demelo/game/MyGame.java
@@ -0,0 +1,15 @@
+package com.demelo.game;
+
+public class MyGame {
+
+	private Game game;
+
+	public MyGame() {
+		game = new Game(800, 600, "My Game");
+		game.getScreenFactory().showScreen(new MyScreen(game.getScreenFactory()));
+	}
+
+	public static void main(String[] args) {
+		new MyGame();
+	}
+}
